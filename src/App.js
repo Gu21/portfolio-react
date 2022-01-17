583681bbc6da9a8be779a0609ddc0a72625482a9
Gu21 <79690181+Gu21@users.noreply.github.com>
2022-01-18 00:22:54 +0100

    mise à jour le 18/01/2022

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import About from './pages/About/About';
 import Projet from './pages/Projet/Projet';
 import Contact from './pages/Contact/Contact';
-
+// import Error from './pages/error/error';
 
 const App = () => {
   return (
@@ -14,6 +14,8 @@ const App = () => {
       <Route path="/history" element={ <About /> } />
       <Route path="/projet-1" element={ <Projet /> } />
       <Route path="/contact" element={ <Contact /> } />
+      <Route path="/" element={<Navigate  to="/" />} />
+      {/* <Route path="*" element={ <Error /> } /> */}
     </Routes>
 
   );
