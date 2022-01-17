f7734ea4e49822e23be71b5ed7b9f84076edcdaa
Gu21 <79690181+Gu21@users.noreply.github.com>
2022-01-17 23:23:28 +0100

    mise à jour le 17/01/2022

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projet from './pages/Projet';
-import Contact from './pages/Contact';
+import Home from "./pages/Home/Home";
+import About from './pages/About/About';
+import Projet from './pages/Projet/Projet';
+import Contact from './pages/Contact/Contact';
 
 
 const App = () => {
   return (
 
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<About />} />
-      <Route path="/projet-1" element={<Projet />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route exact path="/" element={ <Home /> } />
+      <Route path="/history" element={ <About /> } />
+      <Route path="/projet-1" element={ <Projet /> } />
+      <Route path="/contact" element={ <Contact /> } />
     </Routes>
 
   );
